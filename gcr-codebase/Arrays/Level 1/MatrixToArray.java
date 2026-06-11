95fc4ac1b6fda47982de982f730a9e0a104b0d40
Himanshu Choudhary <chaudharyhimanshu276@gmail.com>
2026-06-11 22:11:05 +0530

    [Himanshu] Array Level 1 completed

---
+++ gcr-codebase/Arrays/Level 1/MatrixToArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class MatrixToArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] arr = new int[rows * cols];
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[index++] = matrix[i][j];
+            }
+        }
+
+        System.out.println("1D Array:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
